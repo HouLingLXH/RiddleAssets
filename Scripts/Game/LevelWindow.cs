99c4b6011ee89ec2054ae519f9f9e2b1f829f3eb
HouLingSWJ <30954632+HouLingSWJ@users.noreply.github.com>
2017-09-11 19:07:14 +0800

    添加动画，添加UIBase，

---
+++ Scripts/Game/LevelWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelWindow : UIBase {
+
+    public GameObject btn;
+    private void Start()
+    {
+
+    }
+
+    public override void OnInit()
+    {
+        base.OnInit();
+    }
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+    }
+
+    public void Btn_LevelSelect(int index)
+    {
+        Debug.Log("选择关卡" + index);
+        AnimSystem.Move(btn,from:null,to: Vector3.one * 10000,time:1);
+    }
+
+}
---
+++ Scripts/Game/LoadingWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingWindow : UIBase {
+
+    public override void OnInit()
+    {
+        base.OnInit();
+
+    }
+
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+    }
+
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+    }
+
+}
